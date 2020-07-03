54e914bdeb85c0b85a8f56bffd7a9e9787b31f43
Nicole Wong <nrewong@gmail.com>
2020-07-04 00:07:02 +0900

    add active city selection ability

--- src/containers/active_city.jsx
+++ src/containers/active_city.jsx
@@ -1,1 +1,30 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+
+class ActiveCity extends Component {
+  render() {
+    if (this.props.activeCity) {
+      const { name, address, slug } = this.props.activeCity;
+      const url = `https://kitt.lewagon.com/placeholder/cities/${slug}`;
+      return (
+        <div className="active-city">
+          <h3>{ name }</h3>
+          <p>{ address }</p>
+          <img src={url} alt={name} />
+        </div>
+      );
+    }
+    return <div className="active-city" />
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    activeCity: state.activeCity
+  };
+}
+
+
+export default connect(mapStateToProps, null)(ActiveCity);
