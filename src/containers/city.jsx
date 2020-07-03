353b31f4b113a3a4c6ac7a60bb3946a14dce1fa7
Nicole Wong <nrewong@gmail.com>
2020-07-03 23:51:12 +0900

    add redux to set cities list

--- src/containers/city.jsx
+++ src/containers/city.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import { setActiveCity } from '../actions';
 
 class City extends Component {
+  handleClick = () => {
+    this.props.setActiveCity(this.props.city);
+  }
   render() {
     return (
-      <li className="list-group-item">
+      <li className="list-group-item" onClick={this.handleClick} >
         { this.props.city.name }
       </li>
     );
   }
 }
 
-export default City;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ setActiveCity }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(City);
--- src/containers/city_list.jsx
+++ src/containers/city_list.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import City from './city';
+import { setCities } from '../actions';
 
 class CityList extends Component {
+  componentWillMount() {
+    this.props.setCities();
+  }
   render() {
-    const cities = this.props.cities.map(el => <City city={el} />)
+    const cities = this.props.cities.map(el => <City city={el} key={el.name}/>);
     return (
       <ul className="cities">
         {cities}
@@ -12,4 +18,15 @@ class CityList extends Component {
   }
 }
 
-export default CityList;
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ setCities }, dispatch);
+}
+
+function mapStateToProps(state) {
+  return {
+    cities: state.cities
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CityList);
